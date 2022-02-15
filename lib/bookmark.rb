60879921fb866c54cc02a7a9e8cea828ab733ddc
Rakshak Shetty <rakshak_81@hotmail.com>
2022-02-15 15:57:52 +0000

    Attached DB to the app'

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,8 +1,20 @@
+require 'pg'
 class Bookmark
   def self.all
-    [
-      "http://www.makersacademy.com",
-      "http://www.destroyallsoftware.com",
-      "http://www.google.com"
+    bookmark_arrays = []
+    conn = PG.connect( dbname: 'bookmark_manager', user: 'rakshakshetty' )
+    @records = conn.exec( "SELECT * FROM bookmarks" )
+    @records.each do |bookmark|
+     bookmark_arrays << bookmark['url']
     end
+    bookmark_arrays
+  end
 end
+    
+ 
+
+
+
+# "http://www.makersacademy.com",
+# "http://www.destroyallsoftware.com",
+# "http://www.google.com"
