7e7187048d8ab07ea266298be012772aa3290d1f
Rakshak Shetty <rakshak_81@hotmail.com>
2022-02-15 16:02:16 +0000

    Attached DB

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,13 +1,14 @@
 require 'pg'
 class Bookmark
   def self.all
-    bookmark_arrays = []
+    # bookmark_arrays = []
     conn = PG.connect( dbname: 'bookmark_manager', user: 'rakshakshetty' )
     @records = conn.exec( "SELECT * FROM bookmarks" )
-    @records.each do |bookmark|
-     bookmark_arrays << bookmark['url']
-    end
-    bookmark_arrays
+    @records.map { |bookmark| bookmark['url'] }
+    # @records.each do |bookmark|
+    #  bookmark_arrays << bookmark['url']
+    
+    # bookmark_arrays
   end
 end
     
