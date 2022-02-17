bc46eb65e1da234fa7764e569ac9aee7b8971f49
Jane Parsons <jane.parsons@waitrose.co.uk>
2022-02-17 16:54:19 +0000

    Step 11 - get all tests working after refactoring the model

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -10,23 +10,32 @@ describe Bookmark do
       Bookmark.create('http://www.google.com','google')
 
       bookmarks = Bookmark.all
-      
 
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
-      expect(bookmarks).to include("http://www.google.com")
+      this_bookmark = bookmarks.first
+      
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks.first).to be_a Bookmark
+      expect(bookmarks.first.id).to eq this_bookmark.id
+      expect(bookmarks.first.title).to eq 'makers'
+      expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
+
+      # expect(bookmarks).to include("http://www.makersacademy.com")
+      # expect(bookmarks).to include("http://www.destroyallsoftware.com")
+      # expect(bookmarks).to include("http://www.google.com")
     end 
   end
 
   describe '.create' do
-    it 'adds a new bookmark' do
-      Bookmark.create(url: "https://www.yahoo.com/", title: "Yahoo")
-      expect(Bookmark.all).to include("https://www.yahoo.com/")
-    end
+    # it 'adds a new bookmark' do
+    #   Bookmark.create(url: "https://www.yahoo.com/", title: "Yahoo")
+    #   expect(Bookmark.all).to include("https://www.yahoo.com/")
+    # end
 
     it 'adds a new bookmark' do
-      Bookmark.create(url: "https://www.yahoo.com/", title: "Yahoo").first
-      expect(bookmark['title']).to eq "Yahoo"   
+      # Bookmark.create(url: "https://www.yahoo.com/", title: "Yahoo").first
+      # expect(bookmark['title']).to eq "Yahoo"   
+      this_bookmark = Bookmark.create("https://www.yahoo.com/", "Yahoo").first
+      expect(this_bookmark['title']).to eq "Yahoo"   
     end
   end
 
--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -4,11 +4,11 @@ feature 'Test viewing bookmarks' do
   scenario 'should show list of bookmarks' do
     connection = PG.connect(dbname: 'bookmark_manager_test')
 
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com', 'makers');")
+    # connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    # connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
 
     visit('/bookmarks')
-    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "makers"
   end
 end
